c392eec75096fe5b3d37092cfd6b8dfbea8e074d
Ramzan <ramzand511@gmail.com>
2024-06-26 13:55:39 +0300

    Добавлен запросы

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index()
-    {
-//        return 'hello world';
-       $str = 'fsfsfsf';
-       dd($str);
-    }
+ public function index()
+ {
+     $post = Post::where('is_published', 1)->first();
+     dump($post->title);
+     dd('end');
+ }
+
 }
--- database/migrations/2024_06_25_065605_create_posts_table.php
+++ database/migrations/2024_06_25_065605_create_posts_table.php
@@ -19,7 +19,7 @@ class CreatePostsTable extends Migration
             $table->text('content');
             $table->string('image')->nullable();
             $table->unsignedBigInteger('likes')->nullable();
-            $table->boolean('in_published')->default(1);
+            $table->boolean('is_published')->default(1);
             $table->timestamps();
         });
     }
